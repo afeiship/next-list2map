685cfae6c4d6c82913a31b66996b82e394fc50f8
afeiship <1290657123@qq.com>
2019-06-18 11:10:12 +0800

    add: intiila & test

---
+++ README.md
@@ -0,0 +1,12 @@
+# next-list2map
+> Transform list to map
+
+## installation
+```bash
+npm install -S afeiship/next-list2map --registry=https://registry.npm.taobao.org
+```
+
+## usage
+```js
+//DOCS here!
+```
---
+++ test/test.js
@@ -0,0 +1,42 @@
+var nx = require('next-js-core2');
+require('../src/next-list2map');
+
+test('nx.list2map', function() {
+  var items = [
+    { id: 1, name: 'xiaoming' },
+    { id: 2, name: 'lihua' },
+    { id: 3, name: 'hanmeimei' }
+  ];
+
+  var rs1 = nx.list2map(items, {
+    key: 'id',
+    value: function(i, val) {
+      return val;
+    }
+  });
+
+  var rs2 = nx.list2map(items, {
+    key: function(i, value) {
+      return i + '_' + value.name;
+    },
+    value: function(i, val) {
+      return {
+        username: val.name
+      };
+    }
+  });
+
+  console.log('rs2', rs2);
+
+  expect(rs1).toEqual({
+    '1': { id: 1, name: 'xiaoming' },
+    '2': { id: 2, name: 'lihua' },
+    '3': { id: 3, name: 'hanmeimei' }
+  });
+
+  expect(rs2).toEqual({
+    '0_xiaoming': { username: 'xiaoming' },
+    '1_lihua': { username: 'lihua' },
+    '2_hanmeimei': { username: 'hanmeimei' }
+  });
+});
